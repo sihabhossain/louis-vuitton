fac5adb767842f1434b16b77a024e0b4a484c0b8
Sihab Hossain <sihabhossain380@gmail.com>
2023-10-24 13:22:21 +0600

    navbar fixed

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -6,47 +6,47 @@ import { HiOutlineShoppingBag } from "react-icons/hi";
 const Navbar = () => {
   return (
     <div>
-      <div className=" py-6 px-10 flex items-center justify-between ">
+      <div className=" md:py-6 py-3 md:px-10 px-5 flex items-center justify-between ">
         {/* Menu and Search */}
-        <div className="flex items-center gap-6">
+        <div className="flex items-center md:gap-6">
           {/* menu */}
           <div className="flex items-center gap-3">
             <div>
               <HiOutlineBars3 size={"25px"}></HiOutlineBars3>
             </div>
             <div>
-              <p className="text-[13px]">Menu</p>
+              <p className="text-[13px] hidden md:block">Menu</p>
             </div>
           </div>
 
           {/* search */}
-          <div className="flex items-center gap-3">
+          <div className="flex items-center md:gap-3">
             <div>
               <AiOutlineSearch size={"25px"}></AiOutlineSearch>
             </div>
             <div>
-              <p className="text-[13px]">Search</p>
+              <p className="text-[13px] hidden md:block">Search</p>
             </div>
           </div>
         </div>
 
         {/* Louis vuitton */}
         <div>
-          <h2 className="font-bold text-3xl right-10">LOUIS VUITTON</h2>
+          <h2 className="font-bold text-3xl">LOUIS VUITTON</h2>
         </div>
 
         {/* Call, wishlist , mylv */}
 
         {/* call */}
-        <div className="flex items-center gap-8">
+        <div className="flex items-center md:gap-8">
           <div>
-            <p className="text-[13px]">Call us</p>
+            <p className="text-[13px] hidden md:block">Call us</p>
           </div>
           <div>
-            <p className="text-[13px]">Wishlist</p>
+            <p className="text-[13px] hidden md:block">Wishlist</p>
           </div>
           <div>
-            <p className="text-[13px]">MyLV</p>
+            <p className="text-[13px] hidden md:block">MyLV</p>
           </div>
           <div>
             <HiOutlineShoppingBag size={"20px"}></HiOutlineShoppingBag>
